1acb184c318f4cf6d7a8a23cb80738240087cfa2
ABINDIA1997 <84272094+abindia1997@users.noreply.github.com>
2023-06-27 16:33:21 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'https://main--coruscating-nougat-039503.netlify.app',
-    'https://extraa-value-nextjs.vercel.app/',
+    'https://extraa-value-nextjs.vercel.app',
     'http://localhost:3000'
     // Add other allowed origins here
   ];
